fc3588aa767bc29158df6c73c9144b83325e7667
Irene <iavivas3@gmail.com>
2022-07-11 15:57:28 +0200

    Add user model

---
+++ src/app/models/user.ts
@@ -0,0 +1,20 @@
+export interface iUser {
+  id?: string;
+  name: string;
+  email: string;
+  password: string;
+  role: string;
+  characters: Array<string>;
+  games: Array<string>;
+}
+
+export class UserModel implements iUser {
+  constructor(
+    public name: string,
+    public email: string,
+    public password: string,
+    public role: string,
+    public characters: Array<string>,
+    public games: Array<string>
+  ) {}
+}
