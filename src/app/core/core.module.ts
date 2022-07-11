cf5bf0b016dfd2ad0dbd36237660fc135754e149
Irene <iavivas3@gmail.com>
2022-07-11 15:44:00 +0200

    Generate character and game components

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { MenuComponent } from './menu/menu.component';
+import { CharacterComponent } from './character/character.component';
+import { ListCharacterComponent } from './list-character/list-character.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    MenuComponent
+    MenuComponent,
+    CharacterComponent,
+    ListCharacterComponent
   ],
   imports: [
     CommonModule
