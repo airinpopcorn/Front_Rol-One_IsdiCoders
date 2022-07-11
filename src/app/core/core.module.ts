4566621038f772adc86256ec4d0a25c8d09abc04
Irene <iavivas3@gmail.com>
2022-07-11 12:34:33 +0200

    Generate core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { MenuComponent } from './menu/menu.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    MenuComponent
+  ],
   imports: [
     CommonModule
   ]
