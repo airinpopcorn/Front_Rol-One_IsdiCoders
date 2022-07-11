a4018be3f56b9292dd5c8dc61b7e5589df393269
Irene <iavivas3@gmail.com>
2022-07-11 18:55:16 +0200

    Add routing to game-details

--- src/app/details/game-detail/game-detail-routing.module.ts
+++ src/app/details/game-detail/game-detail-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameDetailComponent } from './game-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: GameDetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class GameDetailRoutingModule { }
+export class GameDetailRoutingModule {}
--- src/app/details/game-detail/game-detail.module.ts
+++ src/app/details/game-detail/game-detail.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { GameDetailRoutingModule } from './game-detail-routing.module';
 import { GameDetailComponent } from './game-detail.component';
-
+import { CoreModule } from 'src/app/core/core.module';
+import { ActivatedRoute } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    GameDetailComponent
-  ],
-  imports: [
-    CommonModule,
-    GameDetailRoutingModule
-  ]
+  declarations: [GameDetailComponent],
+  imports: [CommonModule, GameDetailRoutingModule, CoreModule],
 })
-export class GameDetailModule { }
+export class GameDetailModule implements OnInit {
+  idGame = this.route.snapshot.paramMap.get('id') as string;
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    console.log(this.idGame);
+  }
+}
