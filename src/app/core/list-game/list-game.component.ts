ed10024a016c52133365874a60fc746de3c67691
Irene <iavivas3@gmail.com>
2022-07-11 20:10:07 +0200

    Add basic logic in list components

--- src/app/core/list-game/list-game.component.ts
+++ src/app/core/list-game/list-game.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { GameModel } from 'src/app/models/game';
 
 @Component({
   selector: 'app-list-game',
   templateUrl: './list-game.component.html',
-  styleUrls: ['./list-game.component.css']
+  styleUrls: ['./list-game.component.css'],
 })
 export class ListGameComponent implements OnInit {
+  games!: Array<GameModel>;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
