ad88a954ff2822401bfad798bd50b75fdd4d93ee
Irene <iavivas3@gmail.com>
2022-07-11 15:48:13 +0200

    Generate edit-profile module and component

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -4,16 +4,10 @@ import { CommonModule } from '@angular/common';
 import { ProfileRoutingModule } from './profile-routing.module';
 import { ProfileComponent } from './profile.component';
 import { UserComponent } from './user/user.component';
-
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 @NgModule({
-  declarations: [
-    ProfileComponent,
-    UserComponent
-  ],
-  imports: [
-    CommonModule,
-    ProfileRoutingModule
-  ]
+  declarations: [ProfileComponent, UserComponent, EditProfileComponent],
+  imports: [CommonModule, ProfileRoutingModule],
 })
-export class ProfileModule { }
+export class ProfileModule {}
