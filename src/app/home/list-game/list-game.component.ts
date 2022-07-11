55e8509519d300ce31eb30dce9ceae181c0cd7f2
Irene <iavivas3@gmail.com>
2022-07-11 12:38:51 +0200

    Generate list component

---
+++ src/app/home/list-game/list-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-game',
+  templateUrl: './list-game.component.html',
+  styleUrls: ['./list-game.component.css']
+})
+export class ListGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
