4a9ebac91ddabb9ee97204fef2050c68aa4d599f
Irene <iavivas3@gmail.com>
2022-07-11 18:57:00 +0200

    Import header and menu component in home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  imports: [CommonModule, HomeRoutingModule, CoreModule],
 })
 export class HomeModule {}
