f3396da1256507fef8c7d1181e3a0e4a18d27ce0
Irene <iavivas3@gmail.com>
2022-07-11 15:44:45 +0200

    Generate list-character and list-game components

---
+++ src/app/core/list-character/list-character.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-character',
+  templateUrl: './list-character.component.html',
+  styleUrls: ['./list-character.component.css']
+})
+export class ListCharacterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
