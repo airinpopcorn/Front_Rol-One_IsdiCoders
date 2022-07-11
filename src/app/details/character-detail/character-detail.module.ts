4c5724aa037063fe67331d06644a99edea2fb469
Irene <iavivas3@gmail.com>
2022-07-11 20:11:37 +0200

    Add routing to character and profile component

--- src/app/details/character-detail/character-detail.module.ts
+++ src/app/details/character-detail/character-detail.module.ts
@@ -1,17 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CharacterDetailRoutingModule } from './character-detail-routing.module';
 import { CharacterDetailComponent } from './character-detail.component';
-
+import { CoreModule } from 'src/app/core/core.module';
+import { ActivatedRoute } from '@angular/router';
 
 @NgModule({
-  declarations: [
-    CharacterDetailComponent
-  ],
-  imports: [
-    CommonModule,
-    CharacterDetailRoutingModule
-  ]
+  declarations: [CharacterDetailComponent],
+  imports: [CommonModule, CharacterDetailRoutingModule, CoreModule],
 })
-export class CharacterDetailModule { }
+export class CharacterDetailModule implements OnInit {
+  idCharacter = this.route.snapshot.paramMap.get('id') as string;
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    console.log(this.idCharacter);
+  }
+}
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
-  template: '',
+  template: '<app-user></app-user>',
   styleUrls: [],
 })
 export class ProfileComponent implements OnInit {
