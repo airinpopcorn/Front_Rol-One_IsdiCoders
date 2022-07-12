db846763e2eae9e122fabe118c59f03c6d2b4048
Irene <iavivas3@gmail.com>
2022-07-12 12:57:44 +0200

    Create character reducer

---
+++ src/app/state/character.reducer/character.reducer.ts
@@ -0,0 +1,29 @@
+import { createReducer, on } from '@ngrx/store';
+import { iCharacter } from 'src/app/models/character';
+import * as ac from './character.actions.creators';
+
+export const initialState = {
+  characters: [] as Array<iCharacter>,
+};
+
+export const characterReducer = createReducer(
+  initialState,
+  on(ac.loadCharacters, (state, { characters }) => ({
+    characters: [...characters],
+  })),
+  on(ac.addCharacter, (state, { newCharacter }) => ({
+    characters: [...state.characters, newCharacter],
+  })),
+  on(ac.updateCharacter, (state, { updatedCharacter }) => ({
+    characters: state.characters.map((character) =>
+      character.id === updatedCharacter.id
+        ? { ...character, ...updatedCharacter }
+        : character
+    ),
+  })),
+  on(ac.deleteCharacter, (state, { idDelete }) => ({
+    characters: state.characters.filter(
+      (character) => character.id !== idDelete
+    ),
+  }))
+);
