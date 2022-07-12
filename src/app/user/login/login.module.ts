986ddfae65da932aa7badd519c6357299611b345
Irene <iavivas3@gmail.com>
2022-07-12 18:58:19 +0200

    Change nav menu to apply conditions

--- src/app/user/login/login.module.ts
+++ src/app/user/login/login.module.ts
@@ -4,14 +4,9 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
 
-
 @NgModule({
-  declarations: [
-    LoginComponent
-  ],
-  imports: [
-    CommonModule,
-    LoginRoutingModule
-  ]
+  declarations: [LoginComponent],
+  imports: [CommonModule, LoginRoutingModule],
+  exports: [LoginComponent],
 })
-export class LoginModule { }
+export class LoginModule {}
