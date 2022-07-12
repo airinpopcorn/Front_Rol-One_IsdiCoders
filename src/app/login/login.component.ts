6b5e26d9c2c56671b2d4c97b853ac1cea08562b7
Irene <iavivas3@gmail.com>
2022-07-12 20:07:57 +0200

    Add in loginPage conditional for both options of template, login and register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { ApiUser } from '../services/user.api';
+import { AppState } from '../state/app.state';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  viewRegister!: boolean;
+  constructor(
+    public store: Store<AppState>,
+    public router: Router,
+    public apiUser: ApiUser
+  ) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
+  toggleRegister() {}
 }
