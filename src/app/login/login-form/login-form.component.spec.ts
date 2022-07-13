35a096b551b2767335810ae08c042b01a89e1108
Irene <iavivas3@gmail.com>
2022-07-13 20:55:46 +0200

    Fix testing of login-form

--- src/app/login/login-form/login-form.component.spec.ts
+++ src/app/login/login-form/login-form.component.spec.ts
@@ -1,16 +1,40 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { LoginFormComponent } from './login-form.component';
 
 describe('LoginFormComponent', () => {
+  let mockInitialState = {
+    users: {
+      user: {
+        name: '',
+        email: '',
+        password: '',
+        role: '',
+        characters: [],
+        games: [],
+      },
+      token: '',
+    },
+    characters: {
+      characters: [],
+    },
+    games: {
+      games: [],
+    },
+  };
+  let initialState = mockInitialState;
   let component: LoginFormComponent;
   let fixture: ComponentFixture<LoginFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoginFormComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [LoginFormComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LoginFormComponent);
     component = fixture.componentInstance;
