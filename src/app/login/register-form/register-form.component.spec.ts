a8e145b1b6199ff7d06f0f04b56f2fd6e455b278
Irene <iavivas3@gmail.com>
2022-07-13 20:56:10 +0200

    Fix tests of register-form

--- src/app/login/register-form/register-form.component.spec.ts
+++ src/app/login/register-form/register-form.component.spec.ts
@@ -1,16 +1,40 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { RegisterFormComponent } from './register-form.component';
 
 describe('RegisterFormComponent', () => {
+  let mockInitialState = {
+    users: {
+      user: {
+        name: '',
+        email: '',
+        password: '',
+        role: '',
+        characters: [],
+        games: [],
+      },
+      token: '',
+    },
+    characters: {
+      characters: [],
+    },
+    games: {
+      games: [],
+    },
+  };
+  let initialState = mockInitialState;
   let component: RegisterFormComponent;
   let fixture: ComponentFixture<RegisterFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RegisterFormComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [RegisterFormComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(RegisterFormComponent);
     component = fixture.componentInstance;
