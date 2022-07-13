20035ba98c5a2ef8afce02da78b60f95b3914fce
Irene <iavivas3@gmail.com>
2022-07-13 13:49:28 +0200

    Add menu component testing

--- src/app/core/menu/menu.component.spec.ts
+++ src/app/core/menu/menu.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { MenuComponent } from './menu.component';
 
@@ -8,9 +10,10 @@ describe('MenuComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ MenuComponent ]
-    })
-    .compileComponents();
+      declarations: [MenuComponent],
+      imports: [RouterTestingModule],
+      providers: [provideMockStore()],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(MenuComponent);
     component = fixture.componentInstance;
