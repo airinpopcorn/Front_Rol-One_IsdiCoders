74d4ce6d5f6613de06b51e32def5b47b9b435e18
Irene <iavivas3@gmail.com>
2022-07-14 20:08:01 +0200

    Fix failed tests

--- src/app/details/game-detail/game-detail.component.spec.ts
+++ src/app/details/game-detail/game-detail.component.spec.ts
@@ -1,4 +1,9 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute, convertToParamMap } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
+import { initialState } from 'src/app/state/character.reducer/character.reducer';
 
 import { GameDetailComponent } from './game-detail.component';
 
@@ -8,9 +13,23 @@ describe('GameDetailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ GameDetailComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [GameDetailComponent],
+      providers: [
+        GameDetailComponent,
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: {
+              paramMap: convertToParamMap({
+                id: '1',
+              }),
+            },
+          },
+        },
+        provideMockStore({ initialState }),
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(GameDetailComponent);
     component = fixture.componentInstance;
