bdf9c89f0a9effd0810741866f36c0ce29a386a4
Irene <iavivas3@gmail.com>
2022-07-14 13:02:39 +0200

    Add default route for homePage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,30 +9,34 @@ const routes: Routes = [
   },
   {
     path: MENU_OPTIONS[1].path,
-    loadChildren: () =>
-      import('./players/players.module').then((m) => m.PlayersModule),
+    loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
   {
     path: MENU_OPTIONS[2].path,
     loadChildren: () =>
-      import('./profile/profile.module').then((m) => m.ProfileModule),
+      import('./players/players.module').then((m) => m.PlayersModule),
   },
   {
     path: MENU_OPTIONS[3].path,
+    loadChildren: () =>
+      import('./profile/profile.module').then((m) => m.ProfileModule),
+  },
+  {
+    path: MENU_OPTIONS[4].path,
     loadChildren: () =>
       import('./details/game-detail/game-detail.module').then(
         (m) => m.GameDetailModule
       ),
   },
   {
-    path: MENU_OPTIONS[4].path,
+    path: MENU_OPTIONS[5].path,
     loadChildren: () =>
       import('./details/character-detail/character-detail.module').then(
         (m) => m.CharacterDetailModule
       ),
   },
   {
-    path: MENU_OPTIONS[5].path,
+    path: MENU_OPTIONS[6].path,
     loadChildren: () =>
       import('./login/login.module').then((m) => m.LoginModule),
   },
