6fc1d5a03ba59034c8ed7b33999659d6135f9b08
Irene <iavivas3@gmail.com>
2022-07-14 13:03:22 +0200

    Select from apiGame the game to show in details page

--- src/app/details/game-detail/game-detail.component.ts
+++ src/app/details/game-detail/game-detail.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { iGameModel, iGameState } from 'src/app/models/game';
+import { ApiGame } from 'src/app/services/game.api';
+import { AppState } from 'src/app/state/app.state';
 
 @Component({
   selector: 'app-game-detail',
   templateUrl: './game-detail.component.html',
-  styleUrls: ['./game-detail.component.css']
+  styleUrls: ['./game-detail.component.css'],
 })
 export class GameDetailComponent implements OnInit {
-
-  constructor() { }
+  allGames!: iGameState;
+  filterGame!: {
+    title: string;
+    creator: string;
+    description: string;
+    image: string;
+    characters: Array<string>;
+    template: Object;
+  };
+  idGame = this.route.snapshot.paramMap.get('id') as string;
+  constructor(
+    public route: ActivatedRoute,
+    public store: Store<AppState>,
+    public apiGame: ApiGame
+  ) {}
 
   ngOnInit(): void {
-  }
+    // this.store
+    //   .select((state) => state.games)
+    //   .subscribe({
+    //     next: (data) => {
+    //       this.allGames = data;
+    //     },
+    //   });
 
+    this.apiGame.getOneGame(this.idGame).subscribe({
+      next: (data) => {
+        this.filterGame = data;
+      },
+    });
+
+    // this.filterGame = this.allGames.games.find(
+    //   (game) => game._id === this.idGame
+    // ) as iGameModel;
+  }
 }
