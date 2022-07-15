a2f4572bfde5ac4758826f6d31590a244b924b3e
Irene <iavivas3@gmail.com>
2022-07-15 10:19:34 +0200

    Add routing test of menu.component

--- src/app/core/menu/menu.component.spec.ts
+++ src/app/core/menu/menu.component.spec.ts
@@ -23,4 +23,46 @@ describe('MenuComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  describe('When calling homePage method', () => {
+    it('Should navigate to page home', () => {
+      spyOn(component.router, 'navigate');
+      fixture.detectChanges();
+      component.homePage();
+      expect(component.router.navigate).toHaveBeenCalled();
+    });
+  });
+  describe('When calling playersPage method', () => {
+    it('Should navigate to page players if token exists', () => {
+      component.token = 'token';
+      spyOn(component.router, 'navigate');
+      fixture.detectChanges();
+      component.playersPage();
+      expect(component.router.navigate).toHaveBeenCalled();
+    });
+  });
+  describe('When calling playersPage method', () => {
+    it('Should navigate to page players if token not exist', () => {
+      spyOn(component.router, 'navigate');
+      fixture.detectChanges();
+      component.playersPage();
+      expect(component.router.navigate).toHaveBeenCalled();
+    });
+  });
+  describe('When calling profilePage method', () => {
+    it('Should navigate to page profile if token exists', () => {
+      component.token = 'token';
+      spyOn(component.router, 'navigate');
+      fixture.detectChanges();
+      component.profilePage();
+      expect(component.router.navigate).toHaveBeenCalled();
+    });
+  });
+  describe('When calling playersPage method', () => {
+    it('Should navigate to page players if token not exist', () => {
+      spyOn(component.router, 'navigate');
+      fixture.detectChanges();
+      component.profilePage();
+      expect(component.router.navigate).toHaveBeenCalled();
+    });
+  });
 });
