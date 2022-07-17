c3f4f7a7a0bf4f08f98d4e6042a5a8e16dda1184
Irene <iavivas3@gmail.com>
2022-07-17 19:20:15 +0200

    Add edit character form

--- src/app/forms/edit-char/edit-char.module.ts
+++ src/app/forms/edit-char/edit-char.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { EditCharRoutingModule } from './edit-char-routing.module';
 import { EditCharComponent } from './edit-char.component';
-
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    EditCharComponent
-  ],
-  imports: [
-    CommonModule,
-    EditCharRoutingModule
-  ]
+  declarations: [EditCharComponent],
+  imports: [CommonModule, EditCharRoutingModule, ReactiveFormsModule],
 })
-export class EditCharModule { }
+export class EditCharModule {}
