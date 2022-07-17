fd5110c1a8c528a74249248501b30b66a8f7b5fb
Irene <iavivas3@gmail.com>
2022-07-17 13:40:29 +0200

    Add character detail route

--- src/app/details/character-detail/character-detail-routing.module.ts
+++ src/app/details/character-detail/character-detail-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CharacterDetailComponent } from './character-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: CharacterDetailComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class CharacterDetailRoutingModule { }
+export class CharacterDetailRoutingModule {}
