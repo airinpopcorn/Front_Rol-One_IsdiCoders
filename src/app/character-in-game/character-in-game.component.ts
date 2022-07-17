bca6a51fb1f8f2e182de1b0029d2b18b4a8b462c
Irene <iavivas3@gmail.com>
2022-07-17 13:44:11 +0200

    Adding character in game logic and html to show characters form the game

--- src/app/character-in-game/character-in-game.component.ts
+++ src/app/character-in-game/character-in-game.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { iCharacter } from '../models/character';
+import { ApiGame } from '../services/game.api';
 
 @Component({
   selector: 'app-character-in-game',
   templateUrl: './character-in-game.component.html',
-  styleUrls: ['./character-in-game.component.css']
+  styleUrls: ['./character-in-game.component.css'],
 })
 export class CharacterInGameComponent implements OnInit {
-
-  constructor() { }
+  idGame!: string;
+  charactersGame!: Array<iCharacter>;
+  imageGame!: string;
+  titleGame!: string;
+  constructor(public route: ActivatedRoute, public apiGame: ApiGame) {}
 
   ngOnInit(): void {
+    this.idGame = this.route.snapshot.paramMap.get('id') as string;
+    this.apiGame.getOneGame(this.idGame).subscribe({
+      next: (data) => {
+        this.charactersGame = data.characters;
+        this.imageGame = data.image;
+        this.titleGame = data.title;
+      },
+    });
   }
-
 }
