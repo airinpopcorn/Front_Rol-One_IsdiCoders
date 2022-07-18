9a868b14dadd83dfcd21f8f5c93b73d80e76e359
Irene <iavivas3@gmail.com>
2022-07-18 20:16:11 +0200

    Add testing of header

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -1,16 +1,40 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { provideMockStore } from '@ngrx/store/testing';
 
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
+  let mockInitialState = {
+    users: {
+      user: {
+        name: '',
+        email: '',
+        password: '',
+        role: '',
+        characters: [],
+        games: [],
+      },
+      token: '',
+    },
+    characters: {
+      characters: [],
+    },
+    games: {
+      games: [],
+    },
+  };
+  let initialState = mockInitialState;
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
-    })
-    .compileComponents();
+      imports: [RouterTestingModule, HttpClientTestingModule],
+      declarations: [HeaderComponent],
+      providers: [provideMockStore({ initialState })],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
     component = fixture.componentInstance;
@@ -19,5 +43,23 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('When calling goHome method', () => {
+    it('Should router.navigate be called', () => {
+      spyOn(component.router, 'navigate');
+      fixture.detectChanges();
+      component.goHome();
+      expect(component.router.navigate).toHaveBeenCalled();
+    });
+  });
+  describe('When calling logout method', () => {
+    it('Should call localStorage.clearToken, sotore.dispatch and router.navigate', () => {
+      spyOn(component.localStorage, 'clearToken');
+      spyOn(component.store, 'dispatch');
+      spyOn(component.router, 'navigate');
+      fixture.detectChanges();
+      component.logout();
+      expect(component.localStorage).toHaveBeenCalled();
+    });
   });
 });
