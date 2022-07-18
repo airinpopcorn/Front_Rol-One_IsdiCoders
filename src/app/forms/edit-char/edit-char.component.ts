ec71859cc6f5174a7ea988998c10f9457089c91a
Irene <iavivas3@gmail.com>
2022-07-18 16:52:40 +0200

    Add handleEdit method in component

--- src/app/forms/edit-char/edit-char.component.ts
+++ src/app/forms/edit-char/edit-char.component.ts
@@ -1,3 +1,4 @@
+import { NotExpr } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import {
   AbstractControl,
@@ -6,8 +7,16 @@ import {
   ValidationErrors,
   Validators,
 } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { iCharacter } from 'src/app/models/character';
 import { ApiCharacter } from 'src/app/services/characters.api';
+import { AppState } from 'src/app/state/app.state';
+import {
+  loadCharacters,
+  updateCharacter,
+} from 'src/app/state/character.reducer/character.actions.creators';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-edit-char',
@@ -26,8 +35,10 @@ export class EditCharComponent implements OnInit {
   characterForm!: FormGroup;
   constructor(
     public apiCharacter: ApiCharacter,
+    public store: Store<AppState>,
     public route: ActivatedRoute,
-    public fb: FormBuilder
+    public fb: FormBuilder,
+    public router: Router
   ) {}
 
   ngOnInit(): void {
@@ -35,12 +46,38 @@ export class EditCharComponent implements OnInit {
     this.apiCharacter.getOneCharacter(this.idCharacter).subscribe({
       next: (data) => {
         this.character = data;
-
         Object.entries(this.character).forEach((entry) => {
           this.fbGroupData[entry[0]] = ['', [Validators.required]];
         });
         this.characterForm = this.fb.group(this.fbGroupData);
         this.characterForm.setValue(this.character);
+      },
+    });
+  }
+
+  handleEdit() {
+    this.characterForm.valid;
+    this.apiCharacter.updateCharacter(this.characterForm.value).subscribe({
+      next: (data) => {
+        Swal.fire({
+          icon: 'success',
+          title: 'Yuhuuu...',
+          text: 'You edit your character correctly',
+        });
+        this.store.dispatch(
+          updateCharacter({
+            id: this.idCharacter,
+            updatedCharacter: this.character,
+          })
+        );
+        this.router.navigate(['char-detail/' + this.idCharacter]);
+      },
+      error: (err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Ooops...',
+          text: 'Something went wrong',
+        });
       },
     });
   }
