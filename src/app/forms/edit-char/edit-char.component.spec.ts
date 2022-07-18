93ba729448dcca0db81dcabbd7da5e65413e353a
Irene <iavivas3@gmail.com>
2022-07-18 13:46:18 +0200

    Adding testing of edit character component

--- src/app/forms/edit-char/edit-char.component.spec.ts
+++ src/app/forms/edit-char/edit-char.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { EditCharComponent } from './edit-char.component';
 
@@ -8,9 +11,13 @@ describe('EditCharComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditCharComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule,
+        ReactiveFormsModule,
+      ],
+      declarations: [EditCharComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(EditCharComponent);
     component = fixture.componentInstance;
