46ac9c3cc293b040374cb7b9aa3694325cf36a45
Irene <iavivas3@gmail.com>
2022-07-19 15:55:21 +0200

    Add configuration for sonarCloud

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  SONAR_TOKEN: 'cefcc569282adbd70b623803021688b6a6c332a0',
 };
 
 /*
