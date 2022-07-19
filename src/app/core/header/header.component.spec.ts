285fb906f7d470833a0ba1f2b193c8a52531eb82
Irene <iavivas3@gmail.com>
2022-07-19 13:45:24 +0200

    Update header testing

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -2,6 +2,8 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { provideMockStore } from '@ngrx/store/testing';
+import { LocalStorageService } from 'src/app/services/localStorage.service';
+import { CoreModule } from '../core.module';
 
 import { HeaderComponent } from './header.component';
 
@@ -31,9 +33,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientTestingModule],
+      imports: [RouterTestingModule, HttpClientTestingModule, CoreModule],
       declarations: [HeaderComponent],
-      providers: [provideMockStore({ initialState })],
+      providers: [provideMockStore({ initialState }), LocalStorageService],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HeaderComponent);
@@ -54,12 +56,10 @@ describe('HeaderComponent', () => {
   });
   describe('When calling logout method', () => {
     it('Should call localStorage.clearToken, sotore.dispatch and router.navigate', () => {
-      spyOn(component.localStorage, 'clearToken');
-      spyOn(component.store, 'dispatch');
       spyOn(component.router, 'navigate');
       fixture.detectChanges();
       component.logout();
-      expect(component.localStorage).toHaveBeenCalled();
+      expect(component.router.navigate).toHaveBeenCalled();
     });
   });
 });
